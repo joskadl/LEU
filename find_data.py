9dfa9bc495d587e9a4e1cb0a43f3a7173d95758a
Langen <joska.de.langen@ordina.nl>
2022-08-15 13:14:34 +0200

    add script to yield tree representation of relevant data files in directory

---
+++ find_data.py
@@ -0,0 +1,22 @@
+# TODO: make a tree of data directory, in CSV format, listing .gdb, .shp, .mdb and .gml files (also in .zip)
+from pathlib import Path
+import zipfile
+
+data_directory = Path(r"C:\Users\jla23480\Downloads\Data").glob("**/*")  # Directory containing all data
+filetypes_of_interest = [".shp", ".mdb", ".gml", ".gdb", ".zip"]
+
+
+def list_zip_contents(file: Path) -> list:
+    """Return list of file paths within zip file, for relevant extensions"""
+    with zipfile.ZipFile(file, "r") as f:
+        return [file/x for x in f.namelist() if Path(x).suffix in filetypes_of_interest]
+
+
+# Get all .zip, .shp, .mdb and .gml file paths
+files = [x for x in data_directory if x.is_file() and x.suffix in filetypes_of_interest]
+
+# For all .zip files, replace entry with entry appended with internal file paths wit relevant suffix
+files = [list_zip_contents(file) if file.suffix == ".zip" else file for file in files]
+
+for file in files:
+    print(file)
