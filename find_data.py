4d0b3f9d88751e81fd85dc7a471205cdd629a770
Langen <joska.de.langen@ordina.nl>
2022-08-15 13:38:00 +0200

    feat: show one list of all relevant filepaths, including in zip files

--- find_data.py
+++ find_data.py
@@ -1,4 +1,6 @@
 # TODO: make a tree of data directory, in CSV format, listing .gdb, .shp, .mdb and .gml files (also in .zip)
+from operator import add
+from functools import reduce
 from pathlib import Path
 import zipfile
 
@@ -16,7 +18,10 @@ def list_zip_contents(file: Path) -> list:
 files = [x for x in data_directory if x.suffix in filetypes_of_interest]
 
 # For all .zip files, replace entry with entry appended with internal file paths wit relevant suffix
-files = [list_zip_contents(file) if file.suffix == ".zip" else file for file in files]
+files = [list_zip_contents(file) if file.suffix == ".zip" else [file] for file in files]
+
+# Unpack lists of nested paths from .zip files to one list of all relevant file paths
+files = reduce(add, files)
 
 for file in files:
     print(file)
