5f884e7f055ddaf38b7b101704ff4245b609816f
Langen <joska.de.langen@ordina.nl>
2022-08-29 10:31:19 +0200

    style: apply black formatting to all python files

--- make_file_tree.py
+++ make_file_tree.py
@@ -5,15 +5,19 @@ import zipfile
 
 """This script creates a CSV file displaying a tree of relevant files/folders within a directory"""
 
-data_directory = Path(r"C:\Users\jla23480\Downloads\Data")  # Folder for which to create the tree
-data_directory_contents = data_directory.glob("**/*")  # Contents of directory of interest
+# Folder for which to create the tree:
+data_directory = Path(r"C:\Users\jla23480\Downloads\Data")
+# Contents of directory of interest:
+data_directory_contents = data_directory.glob("**/*")
 filetypes_of_interest = [".shp", ".mdb", ".gml", ".gdb", ".zip"]
 
 
 def list_zip_contents(file: Path) -> list:
     """Return list of file paths with relevant extensions within zip file"""
     with zipfile.ZipFile(file, "r") as f:
-        return [file/x for x in f.namelist() if Path(x).suffix in filetypes_of_interest]
+        return [
+            file / x for x in f.namelist() if Path(x).suffix in filetypes_of_interest
+        ]
 
 
 # Get all .zip, .shp, .mdb and .gml file paths
@@ -34,13 +38,17 @@ length_longest_filepath = max([len(x.parts) for x in files])
 # Create a tree of all relevant folders and files
 separator = ";"  # Separator for CSV file (semicolon for Excel readability)
 previous_path = Path()  # Path to compare next filepath to
-with open('file_tree.csv', 'w', encoding='utf-8') as f:
+with open("file_tree.csv", "w", encoding="utf-8") as f:
     for filepath in files:
-        shared_path_length = len([x for x, y in zip(previous_path.parts, filepath.parts) if x == y])
+        shared_path_length = len(
+            [x for x, y in zip(previous_path.parts, filepath.parts) if x == y]
+        )
 
         # For all new parts in filepath, write a newline for this part, preceded by appropriate number of separators
         for n in range(shared_path_length, len(filepath.parts)):
             # First separator is to make a column for indicating what to do with a file or folder
-            f.write(f"{separator}{separator*n}{filepath.parts[n]}{separator*(length_longest_filepath-n-1)}\n")
+            f.write(
+                f"{separator}{separator*n}{filepath.parts[n]}{separator*(length_longest_filepath-n-1)}\n"
+            )
 
         previous_path = filepath  # Remember filepath to compare next path with
--- map_mac_to_mkle_names.py
+++ map_mac_to_mkle_names.py
@@ -1,12 +1,15 @@
 import geopandas as gpd
 import fiona
 
-gdb_file = './MAC2020_totaal.gdb/'
-klei_names_file = './python_scripts/klei_names.txt'
+gdb_file = "./MAC2020_totaal.gdb/"
+klei_names_file = "./python_scripts/klei_names.txt"
 
-# Make dictionary linking 'Punt', 'Vlak' and 'Lijn' to their KLEi names
+# Make dictionary linking 'Punt', 'Vlak' and 'Lijn' to their KLEi target names
 with open(klei_names_file) as f:
-    klei_names = {line.split(":")[0]: [x.strip() for x in line.split(":")[1].split(",")] for line in f.readlines()}
+    klei_names = {
+        line.split(":")[0]: [x.strip() for x in line.split(":")[1].split(",")]
+        for line in f.readlines()
+    }
 
 # TODO: this should be a loop over all relevant .gdb files later (and shapefiles?)
 # Find all layers in a given .gdb file
@@ -20,14 +23,23 @@ for layer in layers:
     if len(layer_content.geom_type) > 0:  # Ignore layers without geom_type
         geom_type = layer_content.geom_type[0]
         if geom_type not in names:
-            names[geom_type] = [{column_name: "common"} for column_name in layer_content.columns]
+            names[geom_type] = [
+                {column_name: "common"} for column_name in layer_content.columns
+            ]
         else:
             # The intersection of existing common column names and new column names will be marked "common"
-            common_columns = set(list(x.keys())[0] if list(x.values())[0] == "common" else None for x in names[geom_type]).intersection(set(layer_content.columns))
+            common_columns = set(
+                list(x.keys())[0] if list(x.values())[0] == "common" else None
+                for x in names[geom_type]
+            ).intersection(set(layer_content.columns))
             # Iterate over combination of existing and new columns. Assign value "not common" unless in common_columns
-            all_columns = {list(x.keys())[0] for x in names[geom_type]}.union(set(layer_content.columns))
-            names[geom_type] = [{x: "common" if x in common_columns else "not common"} for x in all_columns]
-
+            all_columns = {list(x.keys())[0] for x in names[geom_type]}.union(
+                set(layer_content.columns)
+            )
+            names[geom_type] = [
+                {x: "common" if x in common_columns else "not common"}
+                for x in all_columns
+            ]
 
 
 """
