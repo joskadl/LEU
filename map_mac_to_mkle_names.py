d59dd06bc761d42b502100ba73e069d10f216cba
Langen <joska.de.langen@ordina.nl>
2022-08-29 10:28:08 +0200

    feat: link column names for all geom_types to "common" or "not common"

--- map_mac_to_mkle_names.py
+++ map_mac_to_mkle_names.py
@@ -4,52 +4,30 @@ import fiona
 gdb_file = './MAC2020_totaal.gdb/'
 klei_names_file = './python_scripts/klei_names.txt'
 
-# Get dictionary linking 'Punt', 'Vlak' and 'Lijn' to their KLEi names
+# Make dictionary linking 'Punt', 'Vlak' and 'Lijn' to their KLEi names
 with open(klei_names_file) as f:
     klei_names = {line.split(":")[0]: [x.strip() for x in line.split(":")[1].split(",")] for line in f.readlines()}
 
-# Find all layers in a given .gdb file
 # TODO: this should be a loop over all relevant .gdb files later (and shapefiles?)
+# Find all layers in a given .gdb file
 layers = fiona.listlayers(gdb_file)
 
-# Sort layers by geometry type
-point_layers = []
-line_layers = []
-polygon_layers = []
-empty_layers_attach = []
-empty_layers_other = []
+# Make a dictionary that links geom_type to used names in layer:
+# {"Point": [{"GlobalID": "common"}, {'Gelaagdhei': 'not common'},  ...]}
+names = dict()
 for layer in layers:
     layer_content = gpd.read_file(gdb_file, layer=layer)
-    if len(layer_content.geom_type) > 0:
+    if len(layer_content.geom_type) > 0:  # Ignore layers without geom_type
         geom_type = layer_content.geom_type[0]
-        if geom_type == "Point":
-            point_layers.append(layer)
-        if geom_type == "MultiLineString":
-            line_layers.append(layer)
-        if geom_type == "MultiPolygon":
-            polygon_layers.append(layer)
-    # Sort empty layers by type
-    else:
-        if "ATTACH" in layer:
-            empty_layers_attach.append(layer)
+        if geom_type not in names:
+            names[geom_type] = [{column_name: "common"} for column_name in layer_content.columns]
         else:
-            empty_layers_other.append(layer)
-        # TODO: confirm geometry of empty dataframes (attachments vs empty df columns)
+            # The intersection of existing common column names and new column names will be marked "common"
+            common_columns = set(list(x.keys())[0] if list(x.values())[0] == "common" else None for x in names[geom_type]).intersection(set(layer_content.columns))
+            # Iterate over combination of existing and new columns. Assign value "not common" unless in common_columns
+            all_columns = {list(x.keys())[0] for x in names[geom_type]}.union(set(layer_content.columns))
+            names[geom_type] = [{x: "common" if x in common_columns else "not common"} for x in all_columns]
 
-# TODO: for all points layers, check intersection of column names, and individual additions for each layer
-# TODO: do this for points, lines, polygons
-column_names_sets = []
-for layer in point_layers:
-    layer_content = gpd.read_file(gdb_file, layer=layer)
-    column_names_sets.append(set(layer_content.columns))
-
-common_column_names = set.intersection(*column_names_sets)  # Column names occurring in all point layers
-print(f"{common_column_names=}")
-
-for column_names in column_names_sets:
-    # TODO: incorporate layer name
-    non_common_names = column_names - common_column_names
-    print(sorted(non_common_names))
 
 
 """
